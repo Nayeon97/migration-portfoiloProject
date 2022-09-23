71c3a470f7888b607180714a86948e94671ee471
Naayeon <naaay097@gmail.com>
2022-09-23 23:27:02 +0900

    #2 - [FEAT] : Axios interceptors 적용

---
+++ api/customApi.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosInstance } from 'axios';
+
+const backendPortNumber = '5001';
+const serverUrl =
+  'http://' + window.location.hostname + ':' + backendPortNumber + '/';
+
+export const Axios: AxiosInstance = axios.create({
+  baseURL: `${serverUrl}`,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+Axios.interceptors.request.use(async (config) => {
+  const accessToken = sessionStorage.getItem('userToken');
+  if (config && accessToken) {
+    config.headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+  }
+  return config;
+});
+
+Axios.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  function (error) {
+    return Promise.reject(error.response.data);
+  }
+);
