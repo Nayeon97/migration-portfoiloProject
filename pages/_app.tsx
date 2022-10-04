e9b9ee0e3bf471e2fab208418c3618274f3bef74
Naayeon <naaay097@gmail.com>
2022-10-04 22:25:15 +0900

    #4 - [CHORE] : React-query 설치 및 초기 설정 완료

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,31 @@
 import type { AppProps } from 'next/app';
+import React, { useState } from 'react';
 import { GlobalStyle } from '../styles/global-style';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import Nav from '../components/common/Nav';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  const { dehydratedState } = pageProps as { dehydratedState: unknown };
+
   return (
-    <>
-      <GlobalStyle />
-      <Nav />
-      <Component {...pageProps} />
-      <ToastContainer
-        style={{ fontSize: '15px', marginTop: '30px', width: 'auto' }}
-        position="top-center"
-        autoClose={1200}
-        closeOnClick={true}
-      />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={dehydratedState}>
+        <GlobalStyle />
+        <Nav />
+        <Component {...pageProps} />
+        <ToastContainer
+          style={{ fontSize: '15px', marginTop: '30px', width: 'auto' }}
+          position="top-center"
+          autoClose={1200}
+          closeOnClick={true}
+        />
+        <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
