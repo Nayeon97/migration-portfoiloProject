7a05fcfd4633fb41674ed0807843fe816b8a7142
Naayeon <naaay097@gmail.com>
2022-10-20 15:58:32 +0900

    #2 - [CHORE] : 컴포넌트 타입 수정

--- pages/withAuth.tsx
+++ pages/withAuth.tsx
@@ -1,17 +1,19 @@
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import React from 'react';
+import SnackBar from '../components/common/SnackBar';
 
-const withAuth = (Component: NextPage | JSX.Element) => {
+const withAuth = (Component: NextPage | React.FC) => {
   const Auth = () => {
     const Router = useRouter();
     if (typeof window !== 'undefined') {
-      const accessToken = sessionStorage.getItem('accessToken');
+      const accessToken = sessionStorage.getItem('userToken');
 
       if (!accessToken) {
         Router.replace('/');
-        return null;
+        SnackBar('로그인 후 사용해주세요.', 'error');
       }
-      return Component;
+      return <Component />;
     }
   };
 
